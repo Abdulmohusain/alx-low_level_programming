5230b64595e83e3bbfcd9f2840c3b35f1311e126
Abdullahi Husain <abdulmohusain@gmail.com>
2023-03-13 23:45:29 +0100

    alx

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,10 @@
  * @argv: Argument vector
  * Return: 0 if success
  */
-int main(int argc, char **argv __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
 	return (0);
 }
 
