2b8fd7dbe43621b55c92e11258450644bcd924e5
Abdullahi Husain <abdulmohusain@gmail.com>
2023-03-15 00:48:02 +0100

    alx

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,9 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 
 	if (s1 == NULL)
-		s1[0] = '\0';		
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 	while (s1[lens1])
 		lens1++;
 	while (s2[lens2])
