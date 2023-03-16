637b3acda0b183d1ad5064b63162cb3a1a88334e
Abdullahi Husain <abdulmohusain@gmail.com>
2023-03-16 23:42:41 +0100

    alx

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,17 +7,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-        unsigned int i = 0;
-        char *p;
+	unsigned int i = 0;
+	char *p;
 
-        if (nmemb == 0 || size == 0)
-                return (NULL);
-        p = malloc(size * nmemb);
-        if (!p)
-                return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(size * nmemb);
+	if (!p)
+		return (NULL);
 	for (; i < size * nmemb; i++)
 	{
 		p[i] = 0;
 	}
-        return (p);
+	return (p);
 }
