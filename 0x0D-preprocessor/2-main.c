eb52708ccb721bc7f46cf392f6e6fd560dc0ddd3
Abdullahi Husain <abdulmohusain@gmail.com>
2023-03-21 00:06:50 +0100

    alx

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints the name of the file it was compiled from, followed by a new line.
+ * main - A program that prints the name of the file it was compiled from,
+ * followed by a new line.
+ * Return: 0 on sucess
  */
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
