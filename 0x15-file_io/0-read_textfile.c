a1e2ecb998d16af0739a712ca1bfb0466885c288
Abdullahi Husain <abdulmohusain@gmail.com>
2023-05-08 19:36:10 +0100

    ALX

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * read_textfile - function that reads a text file and
+ * prints it to the POSIX standard output.
+ * @filename: The file name
+ * @letters: Number of letters to be copied.
+ * Return: the actual number of letters it could
+ * read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
