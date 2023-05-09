d96b9f97cba77f227ad9fc249b8df10345e58a3c
Abdullahi Husain <abdulmohusain@gmail.com>
2023-05-09 20:14:35 +0100

    ALX

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: is the name of the file to create
+ * @text_content:  is a NULL terminated string to
+ * write to the file
+ * Returns: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int g = 0;
+	int newFile, writeFile;
+
+	while(text_content[g] != '\0')
+		g++;
+	newFile = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+
+	writeFile = (newFile);
+	if (writeFile == -1)
+		return (-1);
+	if (newFile == -1)
+		return (-1);
+	close(newFile);
+	return (1);
+}
