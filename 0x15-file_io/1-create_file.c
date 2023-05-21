489a8e4c664f03cf3b3abc9d840b4d5e66d15f74
Abdullahi Husain <abdul@DESKTOP-UB1JR4N>
2023-05-21 10:55:14 +0100

    ALX

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,15 +11,18 @@ int create_file(const char *filename, char *text_content)
 	int g = 0;
 	int newFile, writeFile;
 
-	while(text_content[g] != '\0')
-		g++;
 	newFile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	writeFile = (newFile);
-	if (writeFile == -1)
-		return (-1);
 	if (newFile == -1)
 		return (-1);
+	
+	while(text_content[g] != '\0')
+		g++;
+	
+	writeFile = write(newFile, text_content, g);
+	if (writeFile == -1)
+		return (-1);
+	
 	close(newFile);
 	return (1);
 }
