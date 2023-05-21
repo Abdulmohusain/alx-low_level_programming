8a920de4b2a9616d068ecb9f1e872a0a3ad9b621
Abdullahi Husain <abdulmohusain@gmail.com>
2023-05-21 11:16:08 +0100

    ALX

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	while (text_content[g] != '\0')
 		g++;
-	if (g == 0)
+	if (text_content == NULL)
 	{
 		close(newFile);
 		return (1);
