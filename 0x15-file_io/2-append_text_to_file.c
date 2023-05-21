29603f997a383b11ea2aa5f42fe3044b342c392b
Abdullahi Husain <abdulmohusain@gmail.com>
2023-05-21 22:10:17 +0100

    ALX

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	openfl = open(filename, O_WRONLY | O_APPEND | O_EXCL);
+	openfl = open(filename, O_WRONLY | O_APPEND);
 	printf("%d\n", openfl);
 
 	if (openfl == -1)
