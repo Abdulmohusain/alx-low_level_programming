eb157238a922454cc172ccacad7aa424509a11f0
Abdullahi Husain <abdul@DESKTOP-UB1JR4N>
2023-05-21 09:24:39 +0100

    ALX

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	while(text_content[g] != '\0')
 		g++;
-	newFile = open(filename, O_RDWR | O_CREAT | O_TRUNC, "rw-------");
+	newFile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	writeFile = (newFile);
 	if (writeFile == -1)
