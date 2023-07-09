7ee239bf86245bef975074f4e7939d162f0811a5
Abdullahi Husain <abdulmohusain@gmail.com>
2023-07-09 01:12:35 +0100

    ALX

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,12 +11,12 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int a = 1;
 
-	if (index >= 31)
+	if (index > 31)
 		return (-1);
 	a = a << index;
 	if ((n & a) == a) 
 		return (1);
-	else if ((n & a) == 0) 
+	else  
 		return (0);
 	return (-1);
 }
