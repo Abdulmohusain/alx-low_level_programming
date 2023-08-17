6848382176fbb9f60fcd6572c225046f3b50540f
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-17 22:16:19 +0100

    Alx

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end -  function that adds a new node
+ * at the end of a list.
+ * @head: the list
+ * @n: integer data
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *h, *new = malloc(sizeof(dlistint_t));
+
+	h = *head;
+	if (new == NULL)
+		return (NULL);
+	new->next = NULL;
+	new->n = n;
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+	} else
+	{
+		while (h->next)
+		{
+			h = h->next;
+		}
+		h->next = new;
+		new->prev = *head;
+	}
+	return (*head);
+}
---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint -  function that frees a dlistint_t list
+ * @head: the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head->next)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+	free(head);
+}
