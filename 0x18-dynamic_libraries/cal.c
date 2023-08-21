208b07ac9aa90d1b72b5fda5d13d025fa1253e1b
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-21 23:40:55 +0100

    Alx

---
+++ 0x18-dynamic_libraries/cal.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - A function that adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * add - A function that subtract two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * add - A function that multiply two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * add - A function that divide two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * add - A function mod two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
