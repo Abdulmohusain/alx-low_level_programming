1c531c5a612b13e2bdd36619c6c668fcdd59659b
Abdullahi Husain <abdulmohusain@gmail.com>
2023-09-08 15:35:26 -0700

    Alx

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,6 +11,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 
+	if (ht == NULL)
+		return (0);
+	if (strcmp(key, "") == 0);
+		return (0);
 	index = hash_djb2((const unsigned char *)key) % ht->size;
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node  == NULL)
