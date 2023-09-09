ce73f778cb6dcbc180a0f6f08053b4ea6ffb781b
Abdullahi Husain <abdulmohusain@gmail.com>
2023-09-09 06:12:47 -0400

    Alx

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,6 +10,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new_node;
+	hash_node_t *list;
 
 	if (ht == NULL)
 		return (0);
@@ -19,14 +20,28 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node  == NULL)
 		return (0);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
 	if (ht->array[index] == NULL)
 	{
+		new_node->key = strdup(key);
+		new_node->value = strdup(value);
 		new_node->next = NULL;
 		ht->array[index] = new_node;
 		return (1);
 	}
+	list = ht->array[index];
+	while (list)
+	{
+		if (strcmp(key, list->key) == 0)
+		{
+			free(list->value);
+			list->value = strdup(value);
+			free(new_node);
+			return (1);
+		}
+		list = list->next;
+	}
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
