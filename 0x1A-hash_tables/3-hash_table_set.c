d7f06f941ff4be2c6e91e3df879e7faaf029b2d2
Abdullahi Husain <abdulmohusain>
2023-10-06 13:09:14 +0100

    Alx

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,8 +13,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	if (strcmp(key, "") == 0 || strcmp(value, "") == 0)
-		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	new_item = malloc(sizeof(hash_node_t));
