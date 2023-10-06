d0cf44b8dfe2f9c7c8ffb5f74798021bee05fbd2
Abdullahi Husain <abdulmohusain@gmail.com>
2023-10-06 06:36:37 -0700

    Alx

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-
+	if (strcmp(key, "") == 0)
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_item = malloc(sizeof(hash_node_t));
 	if (new_item == NULL)
