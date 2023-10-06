8c5a63fdf6e2ec5848996013ac63d5aa91d780ca
Abdullahi Husain <abdulmohusain@gmail.com>
2023-10-06 09:00:36 -0700

    Alx

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,42 +6,29 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *current;
-	unsigned long int i = 0, j = 0, k = 0, *index;
+	unsigned long int i = 0;
+	hash_node_t *list;
 
 	if (ht == NULL)
-	return;
-
-	index = malloc(sizeof(unsigned long int) * ht->size);
-	if (index == NULL)
-	{
-		printf("malloc failed\n");
 		return;
-	}
 
+	putchar('{');
 	for (; i < ht->size; i++)
 	{
 		if (ht->array[i])
 		{
-			index[j] = i;
-			j++;
+			list = ht->array[i];
+			while (list)
+			{
+				printf("'%s': '%s'", list->key, list->value);
+				if (list->next != NULL)
+				{
+					putchar(',');
+				}
+				list = list->next;
+			}
+
 		}
 	}
-	printf("{");
-	for (k = 0; k < j; k++)
-	{
-		current = ht->array[index[k]];
-		while (current)
-		{
-			printf("'%s': '%s'", current->key, current->value);
-			current = current->next;
-			if (current)
-				printf(", ");
-	}
-		if (k != j - 1)
-			printf(", ");
-	}
 	printf("}\n");
-	free(index);
 }
-
