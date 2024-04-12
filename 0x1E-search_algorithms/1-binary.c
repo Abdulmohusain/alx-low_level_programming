49d8c8b7fa3aea91c2ee3ce02a0058f5023b480b
Abdullahi Husain <abdulmohusain>
2024-04-12 23:05:28 +0100

    Alx

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,7 +13,7 @@ int binary_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	while(bottom <= top)
+	while (bottom <= top)
 	{
 		printf("Searching in array: ");
 		print_array(array, bottom, top);
